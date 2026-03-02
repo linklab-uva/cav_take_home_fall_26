7bc8e1029cc8bc59e7913d677f4c93dd15364b49
fredm150 <mensahfred150@gmail.com>
2026-03-01 23:54:02 -0500

    Completed wheel slip, IMU jitter, and lap time metrics - Frederick Mensah (fjm7se)

---
+++ src/take_home_python/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'take_home_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='freddy07',
+    maintainer_email='freddy07@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'take_home_node = take_home_python.take_home_node:main',
+        ],
+    },
+)
