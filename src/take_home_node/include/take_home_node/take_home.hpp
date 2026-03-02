c97d9cab4756d32c43ef5eae83f3f3cbfc868bd8
fredm150 <mensahfred150@gmail.com>
2026-03-02 00:17:23 -0500

    Completed Tasks A, B, and C (in Python)

--- src/take_home_node/include/take_home_node/take_home.hpp
+++ src/take_home_node/include/take_home_node/take_home.hpp
@@ -11,6 +11,8 @@
 class TakeHome : public rclcpp::Node {
  public:
   TakeHome(const rclcpp::NodeOptions& options);
+  // added line
+  void curvilinear_distance_callback(std_msgs::msg::Float32::ConstSharedPtr msg);
 
   void odometry_callback(nav_msgs::msg::Odometry::ConstSharedPtr odom_msg);
 
@@ -20,4 +22,10 @@ class TakeHome : public rclcpp::Node {
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscriber_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr metric_publisher_;
 
+// Lap time tracking -  added line
+rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr curvilinear_subscriber_;
+rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr lap_time_publisher_;
+double last_curvilinear_distance_ = 0.0;
+rclcpp::Time lap_start_time_;
+bool lap_in_progress_ = false;
 };
